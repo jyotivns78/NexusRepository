01c87fd9254dfae17fc8a92f486c15044d008a44
Jyoti <jyoti@vns.com>
2023-01-19 11:59:20 +0530

    Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-
 const Navbar = () => (
 
-    // <Navbar bg="dark" variant="dark">
-    //     <Container>
-    //       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    //       <Nav className="me-auto">
-    //         <Nav.Link href="#home">Home</Nav.Link>
-    //         <Nav.Link href="#features">Features</Nav.Link>
-    //         <Nav.Link href="#pricing">Pricing</Nav.Link>
-    //       </Nav>
-    //     </Container>
-    //   </Navbar>
 
-    <ul className="nav justify-content-center">
+
+    <ul className="nav justify-content-center" style={{width: "100%", backgroundColor: "gray"}}>
         <li className="me-auto">
-            <Link to="/" className="nav-link" aria-current="page">Home</Link>
+            <Link to="/" className="nav-link" aria-current="page" style={{color:"#fff", fontSize:"20px", fontFamily: "revert", fontWeight: "600"}}>Home</Link>
         </li>
 
         <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/about" className="nav-link" style={{color:"#fff", fontSize:"20px", fontFamily: "revert", fontWeight: "600"}}>About</Link>
         </li>
 
         <li className="nav-item">
-            <Link to="/contact" className="nav-link">Contact</Link>
+            <Link to="/contact" className="nav-link" style={{color:"#fff", fontSize:"20px", fontFamily: "revert", fontWeight: "600"}}>Contact</Link>
         </li>
     </ul>
 )
